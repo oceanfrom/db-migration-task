365053f80d440cd75205482806f13a753e648f34
Andrei Tutski <tutskiandrei9@gmail.com>
2024-11-20 18:41:39 +0100

    added simple migration realisation

--- src/main/java/org/example/MigrationTool.java
+++ src/main/java/org/example/MigrationTool.java
@@ -3,21 +3,23 @@ package org.example;
 import lombok.RequiredArgsConstructor;
 import org.example.manager.MigrationManager;
 import org.example.manager.MigrationTableManager;
+import org.example.service.MigrationService;
 import org.example.utils.ConnectionUtils;
 
 @RequiredArgsConstructor
 public class MigrationTool {
-    private final MigrationManager migrationManager;
+    private final MigrationService migrationService;
 
     public void runMigrations() {
-        migrationManager.createTablesIfNotExist();
+        migrationService.runMigrations();
     }
 
     public static void main(String[] args) {
         ConnectionUtils connectionUtils = new ConnectionUtils();
         MigrationTableManager migrationTableManager = new MigrationTableManager(connectionUtils);
         MigrationManager migrationManager = new MigrationManager(migrationTableManager);
-        MigrationTool tool = new MigrationTool(migrationManager);
+        MigrationService migrationService = new MigrationService(migrationManager, connectionUtils);
+        MigrationTool tool = new MigrationTool(migrationService);
 
         tool.runMigrations();
     }
--- src/main/java/org/example/service/MigrationService.java
+++ src/main/java/org/example/service/MigrationService.java
@@ -1,13 +1,43 @@
 package org.example.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.executor.MigrationExecutor;
 import org.example.manager.MigrationManager;
+import org.example.utils.ConnectionUtils;
+import org.example.utils.MigrationFileReader;
+
+import java.io.File;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class MigrationService {
     private final MigrationManager migrationManager;
+    private final ConnectionUtils connectionUtils;
 
     public void runMigrations(){
         migrationManager.createTablesIfNotExist();
+
+        List<File> successfullyAppliedMigrations = new ArrayList<>();
+        boolean hasErrors = false;
+
+        try(Connection connection = connectionUtils.getConnection()) {
+
+            List<File> migrations = MigrationFileReader.getMigrationFiles();
+
+
+            for(File file : migrations) {
+                MigrationExecutor.executeMigration(file, connection);
+                successfullyAppliedMigrations.add(file);
+            }
+
+            if(!hasErrors) {
+                connection.commit();
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
