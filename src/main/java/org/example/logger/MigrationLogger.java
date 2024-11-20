371f3784ebb3765569ffc87904d3898ddedebdfc
Andrei Tutski <tutskiandrei9@gmail.com>
2024-11-20 18:53:45 +0100

    added logger

---
+++ src/main/java/org/example/logger/MigrationLogger.java
@@ -0,0 +1,28 @@
+package org.example.logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MigrationLogger {
+    private static final Logger logger = LogManager.getLogger(MigrationLogger.class);
+
+    public static void logMigrationStart(String migrationName) {
+        logger.info("The beginning of the migration: {}", migrationName);
+    }
+
+    public static void logMigrationSuccess(String migrationName) {
+        logger.info("Migration was completed successfully: {}", migrationName);
+    }
+
+    public static void logMigrationError(String migrationName, Exception e) {
+        logger.error("Migration execution error: {}. Details: {}", migrationName, e.getMessage(), e);
+    }
+
+    public static void logInfo(String message) {
+        logger.info(message);
+    }
+
+    public static void logError(String message, Exception e) {
+        logger.error(message, e);
+    }
+}
