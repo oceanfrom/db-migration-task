686146245523f5edfe6adc25c943070aa61615b8
Andrei Tutski <tutskiandrei9@gmail.com>
2024-11-20 21:05:00 +0100

    added migration applied logic

--- src/main/java/org/example/manager/MigrationManager.java
+++ src/main/java/org/example/manager/MigrationManager.java
@@ -2,11 +2,27 @@ package org.example.manager;
 
 import lombok.RequiredArgsConstructor;
 
+import java.io.File;
+
 @RequiredArgsConstructor
 public class MigrationManager {
     private final MigrationTableManager migrationTableManager;
+    private final AppliedMigrationManager appliedMigrationManager;
+    private final MigrationHistoryManager migrationHistoryManager;
 
     public void createTablesIfNotExist() {
         migrationTableManager.createTablesIfNotExist();
+    }
+
+    public void logMigrationHistory(String migrationName, String status, String errorMessage) {
+        migrationHistoryManager.logMigrationHistory(migrationName, status, errorMessage);
+    }
+
+    public void markMigrationAsAppleid(File migrationFile, boolean success, String message) {
+        appliedMigrationManager.markMigrationAsApplied(migrationFile, success, message);
+    }
+
+    public boolean isMigrationApplied(File migrationFile) {
+        return appliedMigrationManager.isMigrationApplied(migrationFile);
     }
 }
