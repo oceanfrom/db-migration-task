52691531b870ab4d45c80548efa4e5a24d024fd3
Andrei Tutski <tutskiandrei9@gmail.com>
2024-11-22 12:27:29 +0100

    refactored

--- src/main/java/org/example/executor/MigrationExecutor.java
+++ src/main/java/org/example/executor/MigrationExecutor.java
@@ -1,15 +1,20 @@
 package org.example.executor;
 
+import lombok.RequiredArgsConstructor;
+
 import java.io.File;
 import java.nio.file.Files;
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
+@RequiredArgsConstructor
 public class MigrationExecutor {
 
     public static void executeMigration(File migrationFile, Connection connection) {
-        try (Statement statement = connection.createStatement()) {
+        try {
             String query = Files.readString(migrationFile.toPath());
-            statement.executeUpdate(query);
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.execute();
+            }
         } catch (Exception e) {
             throw new RuntimeException("Failed to execute migration file: " + migrationFile.getName(), e);
         }
